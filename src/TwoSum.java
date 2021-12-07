5bb76e5eb2a29cfc759c470f4b850c26e9122382
oldersg <we@aa.com>
2021-12-07 14:01:02 +0800

    Initial commit

---
+++ src/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2,7,11,15},9)));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int anthor = target - nums[i];
+            if(hashMap.containsKey(anthor))
+            {
+                 return new int[]{hashMap.get(anthor),i};
+            }
+            hashMap.put(nums[i],i);
+        }
+        return null;
+    }
+}
